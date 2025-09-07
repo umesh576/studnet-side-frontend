e6fec92592ab1e8886d6c00fd957eea6920a5421
Umesh Joshi <uj19343@gmail.com>
2025-09-07 21:37:42 +0545

    student portal making

--- src/app/Student-Portal/page.jsx
+++ src/app/Student-Portal/page.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const StudentPortalPage = () => {
-  return <div>StudentPortalPage</div>;
+  return (
+    <div className="w-full">
+      <div className="w-full">
+        <div className="w-full h-0.5">
+          <img
+            src={"/image.png" || "/image.jpg"}
+            alt="image"
+            className="w-full object-fill"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StudentPortalPage;
