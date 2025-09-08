15f24021c75b99bb5d61f1883118b089d4962dcc
Umesh Joshi <uj19343@gmail.com>
2025-09-08 19:23:56 +0545

    working in student page

--- src/app/Student-Portal/page.jsx
+++ src/app/Student-Portal/page.jsx
@@ -2,21 +2,21 @@ import React from "react";
 
 const StudentPortalPage = () => {
   return (
-    <div className="w-full">
+    <div className="w-full bg-blue-100">
       <div className="w-full">
-        <div className="w-full h-[300px]">
+        <div className="w-full h-[100px]">
           <img
             src={"/image.png" || "/image.jpg"}
             alt="image"
-            className="w-full object-contain h-[300px]"
+            className="w-full object-contain h-[200px]"
           />
         </div>
-        <div className="w-1/7">
-          <div className="w-1/2 relative flex justify-center">
+        <div className="w-1/4 flex justify-center rounded-full relative top-20">
+          <div className=" w-1/2 relative flex justify-center">
             <img
               src="/image.jpg"
               alt="image"
-              className="relative top-[-50px]"
+              className="relative rounded-full"
             />
           </div>
         </div>
