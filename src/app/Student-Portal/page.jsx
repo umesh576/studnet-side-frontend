8e7ef9091df6efb2716bce1deef95601248f2f37
Umesh Joshi <uj19343@gmail.com>
2025-09-08 19:20:14 +0545

    student page

--- src/app/Student-Portal/page.jsx
+++ src/app/Student-Portal/page.jsx
@@ -4,12 +4,21 @@ const StudentPortalPage = () => {
   return (
     <div className="w-full">
       <div className="w-full">
-        <div className="w-full h-0.5">
+        <div className="w-full h-[300px]">
           <img
             src={"/image.png" || "/image.jpg"}
             alt="image"
-            className="w-full object-fill"
+            className="w-full object-contain h-[300px]"
           />
+        </div>
+        <div className="w-1/7">
+          <div className="w-1/2 relative flex justify-center">
+            <img
+              src="/image.jpg"
+              alt="image"
+              className="relative top-[-50px]"
+            />
+          </div>
         </div>
       </div>
     </div>
