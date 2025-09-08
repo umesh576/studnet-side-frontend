915f1a1f683556d4d7b91a3d0ad1d659917a5ccd
Umesh Joshi <uj19343@gmail.com>
2025-09-08 22:57:12 +0545

    new commit in student profile

--- src/app/Student-Portal/page.jsx
+++ src/app/Student-Portal/page.jsx
@@ -5,30 +5,33 @@ import { IoCameraOutline } from "react-icons/io5";
 const StudentPortalPage = () => {
   return (
     <div>
-      <div className="w-full bg-blue-100">
-        <div className="w-full bg-blue-100">
-          <div className="w-full h-[100px] py-4 bg-blue-100">
+      <div className="w-full">
+        <div className="w-full bg-transparent">
+          <div className="w-full  py-4 bg-blue-100">
             <img
               src={"/image.png" || "/image.jpg"}
               alt="image"
               className="w-full object-contain h-[200px]"
             />
           </div>
-          <div className="w-1/4 flex justify-center rounded-full relative top-20 bg-blue-100">
-            <div className=" w-1/2  flex justify-center  relative bg-blue-100">
+          <div className="w-1/4 flex justify-center rounded-full relative top-[-130px] bg-transparent border ">
+            <div className=" w-1/2 flex justify-center">
               <img
                 src="/image.jpg"
                 alt="image"
-                className="relative rounded-full bg-blue-100"
+                className="relative rounded-full  "
               />
             </div>
           </div>
-          <div className="relative top-7 left-50 bg-white rounded-full ">
+          <div className="relative  left-50 bg-white rounded-full ">
             <div className="p-1">
               <IoCameraOutline className="text-5xl " />
             </div>
           </div>
         </div>
+      </div>
+      <div className="relative flex top-5 left-32 m-2">
+        <p>umesh joshi</p>
       </div>
       <div className="relative top-5 bg-green-400 w-2/9">
         <StudentActivites />
