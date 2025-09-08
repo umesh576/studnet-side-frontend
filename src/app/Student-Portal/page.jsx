be37599e0a1d778acdfd86b3a46527b50fe40ff2
Umesh Joshi <uj19343@gmail.com>
2025-09-08 21:30:19 +0545

    making student profile

--- src/app/Student-Portal/page.jsx
+++ src/app/Student-Portal/page.jsx
@@ -4,27 +4,29 @@ import { IoCameraOutline } from "react-icons/io5";
 
 const StudentPortalPage = () => {
   return (
-    <div className="w-full bg-blue-100">
-      <div className="w-full">
-        <div className="w-full h-[100px] py-4">
-          <img
-            src={"/image.png" || "/image.jpg"}
-            alt="image"
-            className="w-full object-contain h-[200px]"
-          />
-        </div>
-        <div className="w-1/4 flex justify-center rounded-full relative top-20">
-          <div className=" w-1/2  flex justify-center  relative ">
+    <div>
+      <div className="w-full bg-blue-100">
+        <div className="w-full bg-blue-100">
+          <div className="w-full h-[100px] py-4 bg-blue-100">
             <img
-              src="/image.jpg"
+              src={"/image.png" || "/image.jpg"}
               alt="image"
-              className="relative rounded-full"
+              className="w-full object-contain h-[200px]"
             />
           </div>
-        </div>
-        <div className="relative top-7 left-50 bg-white rounded-full">
-          <div className="p-1">
-            <IoCameraOutline className="text-5xl " />
+          <div className="w-1/4 flex justify-center rounded-full relative top-20 bg-blue-100">
+            <div className=" w-1/2  flex justify-center  relative bg-blue-100">
+              <img
+                src="/image.jpg"
+                alt="image"
+                className="relative rounded-full bg-blue-100"
+              />
+            </div>
+          </div>
+          <div className="relative top-7 left-50 bg-white rounded-full ">
+            <div className="p-1">
+              <IoCameraOutline className="text-5xl " />
+            </div>
           </div>
         </div>
       </div>
--- src/component/student/StudentActivites.jsx
+++ src/component/student/StudentActivites.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const StudentActivites = () => {
-  return <div>StudentAcativites</div>;
+  return (
+    <div>
+      <p>profile</p>
+      <p>Records</p>
+      <p>Notification</p>
+      <p>logout</p>
+    </div>
+  );
 };
 
 export default StudentActivites;
