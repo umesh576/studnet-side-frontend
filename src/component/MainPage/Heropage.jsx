089d10fc41d0e1fe8da869a60867e3845386bfdd
Umesh Joshi <uj19343@gmail.com>
2025-09-21 21:38:16 +0545

    adding new package and start login form handling

--- src/component/MainPage/Heropage.jsx
+++ src/component/MainPage/Heropage.jsx
@@ -9,7 +9,7 @@ const Heropage = () => {
         <div className="h-100 bg-[url('https://media.istockphoto.com/id/171306436/photo/red-brick-high-school-building-exterior.jpg?s=612x612&w=0&k=20&c=vksDyCVrfCpvb9uk4-wcBYu6jbTZ3nCOkGHPSgNy-L0=')] bg-cover bg-center  ">
           <div className="bg-white/30 w-full h-full  flex  items-center">
             <div className=" ">
-              <h1 className="text-8xl font-bold text-blue-500  text-start items-center">
+              <h1 className="text-8xl font-bold text-black  text-start items-center">
                 School of Umesh Technology
               </h1>
               <div className="flex max-sm:block gap-5 w-full mt-5 relative justify-center">
